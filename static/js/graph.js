bee207aa573257ad81affb2607ceb72a5616733d
Katie Maxwell <katie@codeinstitute.net>
2018-02-21 14:07:33 +0000

    initial commit

---
+++ static/js/graph.js
@@ -0,0 +1,10 @@
+queue()
+    .defer(d3.csv, "data/Salaries.csv")
+    .await(makeGraphs);
+
+function makeGraphs(error, salaryData) {
+    var ndx = crossfilter(salaryData);
+    console.log(salaryData)
+
+    dc.renderAll();
+}
