5d7490d4a686da4b21c722d33953023f1941516e
Katie Maxwell <katie@codeinstitute.net>
2018-02-21 14:46:42 +0000

    gender barchart and select menu

--- static/js/graph.js
+++ static/js/graph.js
@@ -3,8 +3,38 @@ queue()
     .await(makeGraphs);
 
 function makeGraphs(error, salaryData) {
-    var ndx = crossfilter(salaryData);
-    console.log(salaryData)
+    
+    let ndx = crossfilter(salaryData);
+    
+    showSelectDiscipline(ndx)
+    showGenderBalance(ndx)
 
     dc.renderAll();
+}
+
+
+function showSelectDiscipline(ndx){
+    let disciplineDim = ndx.dimension(dc.pluck("discipline"));
+    let disciplineGroup = disciplineDim.group();
+    
+    dc.selectMenu("#select_chart")
+        .dimension(disciplineDim)
+        .group(disciplineGroup)
+}
+
+function showGenderBalance(ndx){
+    let genderDim = ndx.dimension(dc.pluck("sex"));
+    let countByGender = genderDim.group().reduceCount();
+    
+    dc.barChart("#gender_chart")
+        .width(400)
+        .height(300)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(genderDim)
+        .group(countByGender)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(20);
 }
